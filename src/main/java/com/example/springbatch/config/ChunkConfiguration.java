446e438a8d8d45c2eb720c4bb5c21e72c699d158
daisy6365 <gksekqls9808@inu.ac.kr>
2024-10-17 16:46:10 +0900

    feat: ChunkOrientedTasklet

--- src/main/java/com/example/springbatch/config/ChunkConfiguration.java
+++ src/main/java/com/example/springbatch/config/ChunkConfiguration.java
@@ -20,6 +20,7 @@ import java.util.Arrays;
 /**
  * ChunkOrientedTasklet
  * Tasklet의 구현체
+ * TaskletStep에 의해 반복적으로 수행
  * Spring Batch가 chunk 기반의 프로세스를 처리할 수 있도록 구현함
  * chunksize 만큼 Transition 안에서 처리
  *
@@ -29,16 +30,23 @@ import java.util.Arrays;
  * 3. chunksize를 기준으로 하나의 item 단위를 생성
  * 4. 하나의 item을 Chunk<I>에 add()
  * 5. Chunk<I>를 반환
+ * ChunkProvider.provide() : ItemReader.read()를 Chunk Size 만큼 반복
  *
  * [ItemProcessor]
  * 1. transform() 호출
  * 2. doProcess()에서 item 가공
  * 3. 가공이 완료된 item을 Chunk<O>에 add()
  * 4. Chunk<O> 반환
+ * ItemProcessor.process() : ItemReader가 전달한 Item 개수 만큼 반복
  *
  * [ItemWriter]
  * 1. write() 호출
  * 2. doWrite에서 최종적으로 DB에 데이터를 처리
+ * ItemWriter.write(items) : 가공된 Items를 일괄 처리
+ *
+ * [Transition]
+ * chunk단위로 맨 마지막에 itemWriter에게 전달 되면, Chunk단위로 commit & rollback.
+ * -> 하나의 chunk마다 새롭게 생성되어 처리
  */
 @Slf4j
 @Configuration
@@ -63,10 +71,20 @@ public class ChunkConfiguration {
          */
         return new StepBuilder("chunkStep1", jobRepository)
                 // input, output, generic Type 을 설정함
-                .<String, String> chunk(5, platformTransactionManager)
+                // chunksize는 commit 간격을 뜻함. chunksize기준으로 reader~writer 반복
+                .<String, String> chunk(2, platformTransactionManager)
                 // 3단계의 Sync 기반 프로세싱 처리
                 // reader, processor, writer를 설정
-                .reader(new ListItemReader<>(Arrays.asList("item1", "item2", "item3", "item4", "item5")))
+                /**
+                 * ChunkOrientedTasklet
+                 * Buffer를 통해 input 데이터를 담아놓음 -> 이후에 삭제함
+                 * Chunk<I> inputs = (Chunk)chunkContext.getAttribute("INPUTS");
+                 * chunkContext.removeAttribute("INPUTS");
+                 *
+                 * 예외가 발생한다면 ?
+                 * 다시 데이터를 읽지 않고, 버퍼에 담아 놓았던 데이터를 가지고옴
+                 */
+                .reader(new ListItemReader<>(Arrays.asList("item1", "item2", "item3", "item4", "item5", "item6")))
                 .processor(new ItemProcessor<String, String>() {
                     @Override
                     public String process(String item) throws Exception {
@@ -75,7 +93,7 @@ public class ChunkConfiguration {
                         log.info("ItemProcessor item = " + item);
 
                         // String 타입의 아이템을 최종적으로 반환해야 함
-                        return "my" + item;
+                        return "my_" + item;
                     }
                 })
                 .writer(new ItemWriter<String>() {
